6c6dcd2945685d2f61f2d64a5f2524a52f42835d
gznznzjsn <arik2046@gmail.com>
2022-03-31 22:16:51 +0300

    DB connected

---
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -0,0 +1,63 @@
+package com.example.sweater;
+
+import com.example.sweater.domain.Message;
+import com.example.sweater.repos.MessageRepos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+    @Autowired
+    private MessageRepos messageRepos;
+
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Map<String, Object> model
+    ) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepos.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepos.save(message);
+        // вообще странно сначала класть и тут же брать
+        Iterable<Message> messages = messageRepos.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String filter(
+            @RequestParam String filter,
+            Map<String, Object> model
+    ) {
+        Iterable<Message> messages;
+        if (filter == null || filter.trim().isEmpty()) {
+            messages = messageRepos.findAll();
+        } else {
+            messages = messageRepos.findByTag(filter);
+        }
+        model.put("messages", messages);
+        return "main";
+    }
+}
