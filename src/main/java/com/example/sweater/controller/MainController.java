b46f92800e4d97d9832be301fd68b329600bdc54
gznznzjsn <arik2046@gmail.com>
2022-04-05 22:18:46 +0300

    +login and registration
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/example/sweater/config/MvcConfig.java
    #	src/main/java/com/example/sweater/config/WebSecurityConfig.java
    #	src/main/resources/templates/greeting.mustache
    #	src/main/resources/templates/login.mustache

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.sweater;
+package com.example.sweater.controller;
 
 import com.example.sweater.domain.Message;
 import com.example.sweater.repos.MessageRepos;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepos messageRepos;
 
