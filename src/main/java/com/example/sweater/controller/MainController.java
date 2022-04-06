d713216a4558e412d1061e118d7abb5c89bf63ab
gznznzjsn <arik2046@gmail.com>
2022-04-07 00:24:21 +0300

    +author for every message

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.sweater.controller;
 
 import com.example.sweater.domain.Message;
+import com.example.sweater.domain.User;
 import com.example.sweater.repos.MessageRepos;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,8 +33,9 @@ public class MainController {
     public String add(
             @RequestParam String text,
             @RequestParam String tag,
+            @AuthenticationPrincipal User user,
             Map<String, Object> model) {
-        Message message = new Message(text, tag);
+        Message message = new Message(text, tag, user);
         messageRepos.save(message);
         // вообще странно сначала класть и тут же брать
         Iterable<Message> messages = messageRepos.findAll();
